77e6959d92c7f1f0640325fc9a893110fc2069f2
Yancy Vance <yancyvance@gmail.com>
2025-01-17 04:22:01 -0500

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -12,15 +12,15 @@
 
 void display_datetime(cdatetime_t *dt) {
     /* This function prints out the entire details of dt using printf().
-     * It follows the format: Month dd, yyyy HH:MM:SS (HH is in 24 hour format 0-23)
+     * It follows the format: Month dd, yyyy HH:MM:SS (HH is in 24-hour format 0-23)
      * For example: January 01, 2025 13:05:45
-     * For the month, it should display the complete month name
+     * For the month, it should display the complete month's name
      * where the first letter is capitalized while the remaining
-     * letters are in lower-case.
+     * letters are in lowercase.
      * Separate the day and year by a comma.
      * For the day, hour, minute, and second components, display
      * a leading zero if there is only a single digit. For example:
-     * 5 should be display as 05; but 15 should be display as 15.
+     * 5 should be displayed as 05; but 15 should be displayed as 15.
      * Lastly, end with the newline character (\n).
     */
 
@@ -29,7 +29,7 @@ void display_datetime(cdatetime_t *dt) {
 
 int compare_datetimes(cdatetime_t *dt1, cdatetime_t *dt2) {
     /* This function compares two cdatetime_t pointers.
-     * It determins which of the two comes first chronologically.
+     * It determines which of the two comes first chronologically.
      * Return:
      *      -1 if dt1 precedes dt2
              1 if dt2 precedes dt1
