f4170ffba8bd8fe59121bf57e80a531d0e55e84c
Yancy Vance <yancyvance@gmail.com>
2025-03-06 11:03:54 -0500

    Update bst.c

--- tree/bst.c
+++ tree/bst.c
@@ -4,7 +4,7 @@
 // Sample C Implementation of a binary search tree.
 // This combines all the codes covered during the lecture.
 // Please report any bug you may find.
-// This code was last updated on 2025-03-06.
+// This code was last updated on 2025-03-03.
 
 typedef struct BSTNode_s {
     int data;
@@ -56,6 +56,33 @@ int main(void) {
     printf("Post-Order Traversal:\n");
     postorder(tree->root);
 
+    BSTNode *tmp;
+
+    // search for a value
+    tmp = search(tree, 90);
+    if(tmp) {
+        printf("Found!\n");
+    }
+    else {
+        printf("Not Found!\n");
+    }
+
+    tmp = search(tree, 5);
+    if(tmp) {
+        printf("Found!\n");
+    }
+    else {
+        printf("Not Found!\n");
+    }
+
+    tmp = search(tree, 75);
+    if(tmp) {
+        printf("Found!\n");
+    }
+    else {
+        printf("Not Found!\n");
+    }
+
     return 0;
 }
 
@@ -105,28 +132,109 @@ void insert(BSTree *tree, int val) {
 
     // find the correct location
     BSTNode *ptr = tree->root;
+    // we need to add another pointer
+    // which holds to old value of ptr
+    // as ptr moves to the next node;
+    // essentially, think of it as the
+    // parent of ptr
+    BSTNode *parent = NULL;
 
-    while(1) {
-        // go to the left?
+    // currently the tree is not empty
+    // so we just have to find the correct
+    // location of the value based on the
+    // BST property; we know that we have
+    // found the correct location of
+    // the new value if that is an empty
+    // spot; otherwise, keep on looking for
+    // an empty spot where we can create
+    // the new node
+    while(ptr != NULL) {
+        // keep on going down the tree
+        // until we find an empty spot!
+
+        // update the parent pointer
+        // because ptr is about to change
+        parent = ptr;
+
+        // update ptr now by determining
+        // whether to go to the left or
+        // right based on the BST property
+        // if what we are about to insert is
+        // less than the current node, we
+        // should go left; otherwise, go right
         if(val < ptr->data) {
-            // check if left is not null
-            if(ptr->left == NULL) {
-                // create the node here
-                ptr->left = create_node(val);
-                return;
-            }
             ptr = ptr->left;
         }
         else {
-            // check if right is not null
-            if(ptr->right == NULL) {
-                // create the node here
-                ptr->right = create_node(val);
-                return;
-            }
             ptr = ptr->right;
         }
     }
+
+    // at this point, we have already found
+    // an empty spot because ptr is already
+    // NULL; what's left to do is to look
+    // at where parent is pointing at because
+    // it will be the parent of the new
+    // node that we will be creating
+    // we just need to know if the node we are
+    // going to create will become either
+    // the left child or the right child
+    // of the parent node; again, BST property
+    if(val < parent->data) {
+        parent->left = create_node(val);
+    }
+    else {
+        parent->right = create_node(val);
+    }
+}
+
+
+BSTNode * search(BSTree *tree, int query) {
+    // assumption is that the data of all
+    // the nodes of the tree is distinct
+
+    // check first if the tree is empty
+    if( is_empty(tree) ) {
+        return NULL;
+    }
+
+    // find the node that has a data
+    // value of what we are looking for
+    // we need a pointer variable that
+    // is moving down the tree; the idea
+    // of a moving pointer is similar to
+    // the ptr variable we used with the
+    // linked list; similarly, we start
+    // pointing at the entry point of the
+    // tree which is the root node
+    BSTNode *ptr = tree->root;
+
+    // keep on moving the pointer
+    // until reach the end of a path;
+    // in short, when ptr points to NULL
+    while(ptr != NULL) {
+        // check if the current node is what
+        // we are looking for
+        if(ptr->data == query) {
+            return ptr;
+        }
+
+        // determine whether to go to
+        // the left or to the right
+        // the BST property determines
+        // our next move (node):
+        // if what we are looking for is
+        // less than the current node, we
+        // should go left; otherwise, go right
+        if(query < ptr->data) {
+            ptr = ptr->left;
+        }
+        else {
+            ptr = ptr->right;
+        }
+    }
+
+    return ptr;
 }
 
 
@@ -135,15 +243,15 @@ void inorder(BSTNode *node) {
     // the root node
     // follows the order: LVR
 
-    // left
+    // left; if there is a left child
     if(node->left != NULL) {
         inorder(node->left);
     }
 
-    // visit
+    // visit the current node
     printf("%d\n", node->data);
 
-    // right
+    // right; if there is a right child
     if(node->right != NULL) {
         inorder(node->right);
     }
@@ -155,15 +263,15 @@ void preorder(BSTNode *node) {
     // the root node
     // follows the order: VLR
 
-    // visit
+    // visit the current node
     printf("%d\n", node->data);
 
-    // left
+    // left; if there is a left child
     if(node->left != NULL) {
         preorder(node->left);
     }
 
-    // right
+    // right; if there is a right child
     if(node->right != NULL) {
         preorder(node->right);
     }
@@ -175,17 +283,17 @@ void postorder(BSTNode *node) {
     // the root node
     // follows the order: LRV
 
-    // left
+    // left; if there is a left child
     if(node->left != NULL) {
         postorder(node->left);
     }
 
-    // right
+    // right; if there is a right child
     if(node->right != NULL) {
         postorder(node->right);
     }
 
-    // visit
+    // visit the current node
     printf("%d\n", node->data);
 }
 
