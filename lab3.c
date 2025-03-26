69e72e70f442b93b1822b288e9dc54c038b055e3
Yancy Vance <yancyvance@gmail.com>
2025-03-25 20:53:54 -0400

    Create lab3.c

---
+++ lab3.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+// COP 3223C Spring 2025
+// Student Name: TODO_your_name_here
+// File Name: pa3_yoursurname.c
+// Note: Rename to use your actual surname. File name should be in lowercase.
+// You must remove any whitespaces or dashes from your surname.
+// Note: **do not** modify or remove TODO comments
+// Do not add other functions or do not call any built-in math functions
+
+// function prototypes
+long factorial(int n);
+long permutation(int n, int r);
+long combination(int n, int r);
+
+
+// DO NOT MODIFY THIS!
+int main(void) {
+    // test factorial; expected output is 120
+    long result_factorial = factorial(5);
+    printf("Factorial of 5 is: %ld\n", result_factorial);
+    
+    // test permutation; expected output is 20
+    long result_permutation = permutation(5, 2);
+    printf("Permutation P(5,2) is: %ld\n", result_permutation);
+    
+    // test combination; expected output is 10
+    long result_combination = combination(5, 2);
+    printf("Combination C(5,2) is: %ld\n", result_combination);
+
+    return 0;
+}
+// END OF DO NOT MODIFY
+
+
+// function definitions
+long factorial(int n) {
+    // TODO 1: Implement this function
+    
+    
+    
+    // End of TODO 1
+}
+
+
+long permutation(int n, int r) {
+    // TODO 2: Implement this function
+    
+    
+
+    // End of TODO 2
+}
+
+
+long combination(int n, int r) {
+    // TODO 3: Implement this function
+    
+    
+
+    // End of TODO 3
+}
