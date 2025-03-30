24aa26db68a5cfcbc3a43392cd80e2e1e1815cff
Yancy Vance <yancyvance@gmail.com>
2025-03-30 18:15:40 -0400

    Update ht_chaining.c

--- hashtable/ht_chaining.c
+++ hashtable/ht_chaining.c
@@ -35,6 +35,8 @@ LList * create_linked_list();
 void destroy_linked_list(LList *);
 void destory_linked_list_recursive(LLNode *);
 void add_to_tail(LList *, char *);
+int hash_function(HashTable *, char *);
+
 
 
 int main(void) {
@@ -163,4 +165,26 @@ void add_to_tail(LList *list, char *val) {
     list->tail->next = tmp;
     // update to have a new tail
     list->tail = tmp;
+}
+
+
+int hash_function(HashTable *hash_table, char *str) {
+    // the following is a simple hash function
+    // that gets the sum of the ascii of the
+    // characters in str; it then maps
+    // the sum to a corresponding number in the
+    // range of 0 to size of hash table, inclusive
+    // this is done through modulo operator
+    int sum_of_ascii = 0;
+
+    // we simply get the sum of the lowercase
+    // version of all the letters of str
+    int len = strlen(str);
+    for(int i = 0; i < len; i++)
+        sum_of_ascii = sum_of_ascii + str[i];
+
+    // afterward, we do a division to the sum
+    // so that the result will be in the range
+    // of 0-capacity of the hash table
+    return sum_of_ascii % hash_table->capacity;
 }
